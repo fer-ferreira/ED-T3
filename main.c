d63640550efa2e93d569433fc77e1506b0c71c70
Fernanda Ferreira <fernanda.ferreira@catijr.com.br>
2018-07-05 19:08:09 -0300

    Adiciona estrutura de dado 'árvore binária' e suas operações

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "binaryTree.h"
+#include "BinaryTree.h"
 
 int main()
 {
-    printf("\n----- Binary Tree RPG Game -----\n");
+    //Programa teste
+    TipoArvore *tree;
+    char *frase1 = "Binary Tree RPG Game";
+    char *frase2 = "Left node";
+    char *frase3 = "Right node";
+
+
+    CriaArvore(&tree);
+    CriaNo(&tree, frase1);
+    Visita(tree);
+
+    InsereEsquerda(tree, frase2);
+    InsereDireita(tree, frase3);
+    VisitaDireita(tree);
+    VisitaEsquerda(tree);
+
     return 0;
 }
